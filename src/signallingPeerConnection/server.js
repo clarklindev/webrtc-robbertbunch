8bac6b52c7aaeb4776ddf733bb01091ec8e88ad7
clarklindev <clarklindev@gmail.com>
2025-01-28 21:15:15 +0800

    27. emit newOffer

--- src/signallingPeerConnection/server.js
+++ src/signallingPeerConnection/server.js
@@ -22,4 +22,32 @@ const io = socketio(expressServer);
 expressServer.listen(8181);
 io.on('connection', (socket)=>{
   console.log('someone has connected');
+
+  const userName = socket.handshake.auth.userName;
+  const password = socket.handshake.auth.password;
+
+  if(password !== 'x'){
+    socket.disconnect(true);
+    return;
+  }
+
+  connectSockets.push({
+    socketId: socket.id,
+    userName
+  });
+
+  socket.on('newOffer', newOffer=> {
+    offers.push({
+      offererUserName: userName, 
+      offer: newOffer, 
+      offererIceCandidates:[],
+      answererUserName:null,
+      answer:null,
+      answererIceCandidates:[]
+    });
+
+    //send out to all connected sockets EXCEPT the caller
+    socket.broadcast.emit('newOfferAwaiting', offers.slice(-1));
+  });
+
 });
